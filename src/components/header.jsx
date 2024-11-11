9fc31b8a27dc5d921c7ec09d83d51cc5d4656178
Felicio <feliciorar@gmail.com>
2024-11-11 09:01:26 -0300

    fix message

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 export default function Header({ children, title }) {
     return (
         <header className="max-lg:px-4 max-lg:py-6 py-4 px-2 h-20 max-w-screen-xl w-full text-3xl flex items-center justify-between">
-            <Link href="/" className="bg-none font-semibold">{title}</Link>
+            <Link href="/" className="bg-none font-semibold max-sm:hidden">{title}</Link>
+            <Link href="/" className="bg-none font-semibold sm:hidden">Olá!</Link>
             <div>
                 {children}
             </div>
